7236965bb5119ba2b398a55b9d8306ce383b74da
abdurrahman <arahmanmh15@gmail.com>
2024-05-10 10:12:12 +0800

    first commit

---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello = () => {
+    // return(
+    //     <div>
+    //         <h1>Hello JSX</h1>
+    //     </div>
+    // )
+    return React.createElement('div',null,React.createElement('h1',null, "Hello this is JSX)"))
+}
+export default Hello
