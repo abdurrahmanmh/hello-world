7d08fa1d4582e2a8ce21766e38c80fc6f7dd4db2
abdurrahman <arahmanmh15@gmail.com>
2024-05-10 15:36:19 +0800

    styling and css

--- src/App.js
+++ src/App.js
@@ -10,10 +10,18 @@ import ClassClick from './components/ClassClick';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from "./appStyles.module.css";
 
 function App() {
   return (
     <div className="App">
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>success</h1>
+      <Inline/>
+      <Stylesheet primary={true}/>
       <NameList/>
       <UserGreeting/>
       <ParentComponent/>
