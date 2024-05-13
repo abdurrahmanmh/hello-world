37e7eb90ac2bfda45857a9cd5d92d15dc33748a8
abdurrahman <arahmanmh15@gmail.com>
2024-05-13 13:10:18 +0800

    component update learn

--- src/components/LifecycleB.js
+++ src/components/LifecycleB.js
@@ -1,33 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class LifecycleB extends Component {
- constructor(props) {
-   super(props)
- 
-   this.state = {
-      name:'wish wash'
-   }
-   console.log("Lifecycle B constructor")
- }
- static getDerivedStateFromProps(props,state){
-    console.log("Lifecycle B getDerivedStateFromProps")
+class LifecycleB extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "wish wash",
+    };
+    console.log("Lifecycle B constructor");
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log("Lifecycle B getDerivedStateFromProps");
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("Lifecycle B componentDIdMount");
+  }
+
+  shouldComponentUpdate() {
+    console.log("LifecycleB shoudld Component Update");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("LifecycleBget getSnapshotBefore Update");
     return null
- }
+  }
 
- componentDidMount(){
-    console.log('Lifecycle B componentDIdMount')
- }
+  componentDidUpdate() {
+    console.log("Lifecycle componentDidUpdate");
+  }
 
-
-
-    render() {
-        console.log('Lifecyle A render')
-    return (
-      <div>
-        LifecycleB
-      </div>
-    )
+  render() {
+    console.log("Lifecyle B render");
+    return <div>LifecycleB</div>;
   }
 }
 
-export default LifecycleB
+export default LifecycleB;
