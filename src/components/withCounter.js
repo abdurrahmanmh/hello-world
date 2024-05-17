305a916660356ae431f18ee55eb59c960dc9ddf5
abdurrahman <arahmanmh15@gmail.com>
2024-05-17 14:55:52 +0800

    part 2

---
+++ src/components/withCounter.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const withCounter = (WrappedComponent) => {
+  class WithCounter extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        count: 0,
+      };
+    }
+    incrementCount = () => {
+      this.setState((prevState) => {
+        return { count: prevState.count + 1 };
+      });
+    };
+
+    render() {
+      return (
+        <WrappedComponent
+          name="wish wash"
+          count={this.state.count}
+          incrementCount={this.incrementCount}
+        />
+      );
+    }
+  }
+  return WithCounter;
+};
+
+export default withCounter;
+
+// export default (WrappedComponent) => {
+//   const hocComponent = ({ ...props }) => <WrappedComponent {...props} />
+
+//   hocComponent.propTypes = {}
+
+//   return hocComponent
+// }
